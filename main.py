c3bd4dab5a2b931bcbbc86caf9529384f1d75468
linho <linho301150@gmail.com>
2023-07-12 22:38:01 +0900

    [feat/#2]✨Add: Setting Interceptor Middleware for Logging System

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from src.oci_function import router as oci_function_router
+from middleware.interceptor import SealAPIMiddleware
 
 app = FastAPI()
+app.add_middleware(SealAPIMiddleware)
 
 
 @app.get("/")
---
+++ middleware/interceptor.py
@@ -0,0 +1,15 @@
+from fastapi import Request, Response
+from starlette.middleware.base import RequestResponseEndpoint
+from starlette.middleware.base import BaseHTTPMiddleware
+
+
+class SealAPIMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        #TODO: Currently in development for logging system.
+        res = await call_next(request)
+        if res.headers.get('content-type') == 'application/json':
+            return res
+        return res
+
+    async def seal_json(self, res):
+        print(type(res))
