aa4cdd00f0954c5c7c74ff70bd7bdaff66e287e2
Nayeon Keum <nkeum@lablup.com>
2023-07-25 01:22:44 +0900

    [feat/#5] :sparkles: Add: mount and use oci-fn-res config files

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,8 @@
 # oci config permission setting
 chmod 600 /root/.oci/*
 
+# config oci fn
+curl -LSs https://raw.githubusercontent.com/fnproject/cli/master/install | sh
+
 # api server start
 cd /app && python3 -m uvicorn app.main:app --reload --host="0.0.0.0" --port=80
