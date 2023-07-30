bcc4d034a18c30ebf286b8b33614c149d021d883
geonwoo <kgw7401@gmail.com>
2023-07-30 16:01:39 +0900

    [feat/#9]✨ Add: knative func in container

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,5 +9,14 @@ curl -LSs https://raw.githubusercontent.com/fnproject/cli/master/install | sh
 # config oci docker login
 docker login -u 'idhxkx7uajar/ryann3@sookmyung.ac.kr' iad.ocir.io -p $DOCKER_PWD
 
+# config kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# func permission setting
+curl -Lo knative_func  "https://github.com/knative/func/releases/download/knative-v1.11.0/func_linux_arm64"
+mv knative_func /usr/local/bin/func
+chmod +x /usr/local/bin/func
+
 # api server start
 cd /app && python3 -m uvicorn app.main:app --reload --host="0.0.0.0" --port=80
