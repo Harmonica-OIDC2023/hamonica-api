ee901b96e0224b1b42f13e52bc5c1b60280e3608
linho <linho301150@gmail.com>
2023-08-06 13:50:39 +0900

    [feat/#4]🐛Fix: include module with router

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ from fastapi import FastAPI
 
 from dto.oci_cli_dto import OciCliVersionDto
 from middleware.interceptor import SealAPIMiddleware
+from src.oci_api_gateway.oci_api_gateway_router import router as oci_api_gateway_router
+from src.oci_application.oci_application_router import router as oci_application_router
+from src.oci_function.oci_function_router import router as oci_function_router
 
 app = FastAPI()
+app.include_router(oci_function_router)
+app.include_router(oci_application_router)
+app.include_router(oci_api_gateway_router)
 app.add_middleware(SealAPIMiddleware)
 
 
