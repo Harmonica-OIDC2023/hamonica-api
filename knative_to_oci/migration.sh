ec68700688eeb1b0d8dd870be2c321e5145fee27
geonwoo <kgw7401@gmail.com>
2023-08-08 00:26:20 +0900

    [feat/#19] ✨ Add:  knative to oci migration code

---
+++ knative_to_oci/migration.sh
@@ -0,0 +1,18 @@
+source .env
+
+fn init --runtime python \
+        --entrypoint "/python/bin/fdk /function/func.py main" \
+        $FUNCTION_NAME
+
+python3 migration.py
+
+mv ./func.py $FUNCTION_NAME/func.py
+mv ./requirements.txt $FUNCTION_NAME/requirements.txt
+
+oci fn application create -c $COMPARTMENT_ID \
+                        --display-name $FNAPP_NAME \
+                        --subnet-ids '["'"${SUBNET_ID}"'"]'
+
+cd $FUNCTION_NAME
+
+fn deploy --app $FUNCTION_NAME
