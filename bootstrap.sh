bc8224fa4970a343b70c43d732fd7dbc417ced52
linho <linho301150@gmail.com>
2023-08-11 04:56:08 +0900

    [Feat/#21]✨Feat: OCI FUNCTION 모듈 개발

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,4 +19,4 @@ mv knative_func /usr/local/bin/func
 chmod +x /usr/local/bin/func
 
 # api server start
-cd /app && python3 -m uvicorn app.main:app --reload --host="0.0.0.0" --port=80
+cd /app && python3 -m uvicorn main:app --host="0.0.0.0" --port=80
--- src/oci_function/dto/oci_function_dto.py
+++ src/oci_function/dto/oci_function_dto.py
@@ -3,7 +3,12 @@ from pydantic import BaseModel
 
 class DeployFunctionDto(BaseModel):
     fnapp_name: str
+    function_name: str
 
 
 class GetFunctionListDto(BaseModel):
     fnapp_id: str
+
+
+class CreateFunctionDto(BaseModel):
+    function_name: str
