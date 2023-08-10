a285e1d3c2a3722dc6a210ff50c6a40bdab25db3
linho <linho301150@gmail.com>
2023-08-11 04:56:08 +0900

    [Feat/#21]✨Feat: OCI FUNCTION 모듈 개발

--- src/oci_function/oci_function_router.py
+++ src/oci_function/oci_function_router.py
@@ -1,18 +1,34 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 
-from src.oci_function.dto.oci_function_dto import DeployFunctionDto, GetFunctionListDto
+from src.oci_function.dto.oci_function_dto import CreateFunctionDto, DeployFunctionDto
 from src.oci_function.oci_function_services import OciFunctionServices
 
 router = APIRouter(prefix="/api/v1/oci-functions", tags=["oci-functions"])
 
 
 @router.post(path="/")
-async def create_oci_application(deploy_function_dto: DeployFunctionDto):
+async def create_oci_function(create_function_dto: CreateFunctionDto):
     oci_functions_services = OciFunctionServices()
-    return [oci_functions_services.deploy_function(deploy_function_dto)]
+    oci_functions_services.create_function_in_oci(create_function_dto.function_name)
+    return Response(status_code=200)
+
+
+@router.post(path="/deploy")
+async def deploy_oci_function(deploy_function_dto: DeployFunctionDto):
+    oci_functions_services = OciFunctionServices()
+    oci_functions_services.deploy_function_by_id(
+        deploy_function_dto.fnapp_name, deploy_function_dto.function_name
+    )
+    return Response(status_code=200)
 
 
 @router.get(path="/")
-async def get_oci_application_list(fnapp_id: str):
+async def get_oci_function_by_id(fnapp_id: str):
     oci_functions_services = OciFunctionServices()
-    return [oci_functions_services.get_function_list(fnapp_id)]
+    return [oci_functions_services.get_function_by_id(fnapp_id)]
+
+
+@router.post(path="/init")
+async def get_oci_function_by_id(function_name: str):
+    oci_functions_services = OciFunctionServices()
+    return [oci_functions_services.init_function_by_function_name(function_name)]
--- src/oci_function/oci_function_services.py
+++ src/oci_function/oci_function_services.py
@@ -1,19 +1,61 @@
 import json
 import os
 
+import dotenv
+from fastapi import HTTPException
+
+dotenv_file = dotenv.find_dotenv()
+dotenv.load_dotenv(dotenv_file)
+
 
 def execute_command_with_json(command):
-    result = os.popen(command).read().strip()
-    return json.loads(result)
+    try:
+        result = os.popen(command).read().strip()
+        return json.loads(result)
+    except Exception as err:
+        raise HTTPException(status_code=404, detail=f"Wrong Input: {err}")
+
+
+def setting_dotenv_with_key(dataset, key_name, key_id):
+    try:
+        for data in dataset["data"]:
+            if data["display-name"] == os.environ[key_name]:
+                dotenv.set_key(dotenv_file, key_id, data["id"])
+                break
+        return dataset, data["id"]
+    except Exception as err:
+        raise HTTPException(status_code=404, detail=f"Not Found: {err}")
 
 
 class OciFunctionServices:
     @staticmethod
-    def deploy_function(fnapp_name: str):
-        command = f"fn deploy --app {fnapp_name}"
-        return execute_command_with_json(command)
+    def create_function_in_oci(function_name: str):
+        command = f"func create -l python {function_name}"
+        os.popen(command).read().strip()
+        return
 
     @staticmethod
-    def get_function_list(fnapp_id: str):
+    def deploy_function_by_id(fnapp_name: str, function_name: str):
+        command = f"fn deploy --app {fnapp_name} --working-dir /app/{function_name}"
+        os.popen(command).read().strip()
+        return
+
+    @staticmethod
+    def get_function_by_id(fnapp_id: str):
         command = f"oci fn function list --application-id {fnapp_id} --all"
-        return execute_command_with_json(command)
+        function_list = execute_command_with_json(command)
+        result, function_id = setting_dotenv_with_key(
+            dataset=function_list, key_name="FNFNC_NAME", key_id="FNFNC_ID"
+        )
+        f = open("./oci-cli/apideploy-spec.json", "r")
+        file = f.read().replace("FUNCTION_ID", function_id)
+        f.close()
+        f = open("./oci-cli/apideploy-spec.json", "w")
+        f.write(file)
+        f.close()
+        return result
+
+    @staticmethod
+    def init_function_by_function_name(function_name: str):
+        command = f"""fn init --runtime python --entrypoint "/python/bin/fdk /function/func.py main" {function_name}"""
+        return os.popen(command).read().strip()
